83770b60c3f13ec0d5aaa3bd12d6581b77ec957e
Tim Bryan <tim.bryan@drpgroup.com>
2022-07-06 08:54:30 +0100

    use most recent data for BlogGallery

--- src/components/PublishDate.tsx
+++ src/components/PublishDate.tsx
@@ -23,7 +23,7 @@ function PublishDate({
     );
   }
 
-  return <span>{format(new Date(posted), AppConfig.dateFormat)}</span>;
+  return <span>{format(new Date(updated), AppConfig.dateFormat)}</span>;
 }
 
 export default PublishDate;
