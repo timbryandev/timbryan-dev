36baa8230816671f844a7200678af0135b340f98
Tim Bryan <tim.bryan@drpgroup.com>
2022-07-19 16:04:30 +0100

    add rss links to footer

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -15,50 +15,55 @@ const Main = (props: IMainProps) => (
     {props.meta}
 
     <div className="max-w-screen-md mx-auto">
-      <div className="text-center pt-16 pb-8">
-        <div className="font-semibold text-3xl text-gray-900 dark:text-gray-300">
+      <header className="text-center pt-16 pb-8">
+        <h1 className="font-semibold text-3xl text-gray-900 dark:text-gray-300">
           {AppConfig.author}
-        </div>
+        </h1>
         <div className="text-xl">{AppConfig.description}</div>
-      </div>
-      <div>
-        <Navbar>
-          <li className="mr-6">
-            <Link href="/">
-              <a>Posts</a>
-            </Link>
-          </li>
-          <li className="mr-6">
-            <Link href="/about/">
-              <a>About</a>
-            </Link>
-          </li>
-          <li className="mr-6">
-            <Link href="/contact/">
-              <a>Contact</a>
-            </Link>
-          </li>
-        </Navbar>
-      </div>
+      </header>
+
+      <Navbar>
+        <li className="mr-6">
+          <Link href="/">
+            <a>Posts</a>
+          </Link>
+        </li>
+        <li className="mr-6">
+          <Link href="/about/">
+            <a>About</a>
+          </Link>
+        </li>
+        <li className="mr-6">
+          <Link href="/contact/">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </Navbar>
 
       <hr />
 
-      <div className="text-xl py-5">{props.children}</div>
+      <main className="text-xl py-5">{props.children}</main>
 
       <hr />
 
-      <div className="text-center py-8 text-sm">
-        © Copyright {new Date().getFullYear()}{' '}
-        <Link href={AppConfig.url}>{AppConfig.author}</Link>. Project base from
-        the awesome <span role="img" aria-label="Love"></span>{' '}
-        <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a> ♥
-        {/*
-         * PLEASE READ THIS SECTION
-         * We'll really appreciate if you could have a link to our website
-         * The link doesn't need to appear on every pages, one link on one page is enough.
-         * Thank you for your support it'll mean a lot for us.
-         */}
-      </div>
+      <footer className="py-8 text-sm">
+        <p>
+          RSS feeds: <Link href="/rss/feed.xml">XML</Link>,{' '}
+          <Link href="/rss/feed.json">JSON</Link>
+        </p>
+        <p className="text-center ">
+          © Copyright {new Date().getFullYear()}{' '}
+          <Link href={AppConfig.url}>{AppConfig.author}</Link>. Project base
+          from the awesome <span role="img" aria-label="Love"></span>{' '}
+          <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a> ♥
+          {/*
+           * PLEASE READ THIS SECTION
+           * We'll really appreciate if you could have a link to our website
+           * The link doesn't need to appear on every pages, one link on one page is enough.
+           * Thank you for your support it'll mean a lot for us.
+           */}
+        </p>
+      </footer>
     </div>
   </div>
 );
