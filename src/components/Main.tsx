e35ffaaae7166279b21739330777f6efa8b0b595
Tim Bryan <tim.bryan@drpgroup.com>
2022-09-08 08:18:35 +0100

    move hero styles

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { BiCoffeeTogo } from 'react-icons/bi';
+import { MdRssFeed } from 'react-icons/md';
 
 import { AppConfig } from '../utils/AppConfig';
 import { NavBar } from './NavBar';
@@ -22,21 +24,19 @@ const Main = (props: IMainProps) => (
       </div>
 
       <Link href="/">
-        <header className="text-center m-auto mt-8 bg-[#f8f6f5] dark:bg-[#090a0e] rounded-lg p-2 pb-5 cursor-pointer shadow hover:scale-105 transition-all">
-          <h1 className="font-semibold text-3xl text-gray-900 dark:text-gray-300">
-            {AppConfig.author}
-          </h1>
-          <div style={{ margin: 'auto', maxWidth: '180px' }}>
-            <Image
-              src="/assets/images/avatar.jpg"
-              className="rounded-full "
-              alt="Avatar photo of Tim smiling in a blue polo shirt against a blurred bubble background"
-              width="500"
-              height="500"
-              layout="intrinsic"
-            />
-          </div>
-          <div className="text-xl">{AppConfig.description}</div>
+        <header className="hero">
+          <h1 className="hero__title">{AppConfig.author}</h1>
+
+          <Image
+            src="/assets/images/avatar.jpg"
+            className="hero__image"
+            alt="Avatar photo of Tim smiling in a blue polo shirt against a blurred bubble background"
+            width="180"
+            height="180"
+            layout="intrinsic"
+          />
+
+          <div className="hero__text">{AppConfig.description}</div>
         </header>
       </Link>
 
@@ -48,10 +48,12 @@ const Main = (props: IMainProps) => (
 
       <footer className="py-8">
         <p className="text-lg">
-          <a href="https://ko-fi.com/S6S5EHTC8">Buy Tim a coffee ☕</a>
+          <a href="https://ko-fi.com/S6S5EHTC8">
+            <BiCoffeeTogo /> Buy Tim a coffee
+          </a>
         </p>
         <p>
-          RSS feeds: <Link href="/rss/feed.xml">XML</Link>,{' '}
+          <MdRssFeed /> RSS feeds: <Link href="/rss/feed.xml">XML</Link>,{' '}
           <Link href="/rss/feed.json">JSON</Link>
         </p>
         <p>
@@ -60,9 +62,12 @@ const Main = (props: IMainProps) => (
             @juanjodev02
           </a>
         </p>
+        <p>&nbsp;</p>
         <p className="text-center ">
           © Copyright {new Date().getFullYear()}{' '}
-          <Link href={AppConfig.url}>{AppConfig.author}</Link>.<br />
+          <Link href={AppConfig.url}>{AppConfig.author}</Link>.
+        </p>
+        <p>
           Base project from the awesome{' '}
           <span role="img" aria-label="Love"></span>{' '}
           <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a> ❤️
