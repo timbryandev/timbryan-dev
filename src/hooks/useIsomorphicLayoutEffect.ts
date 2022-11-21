98e8f7c40ccfce593cf28a41d6ed4534ce8631e7
Tim Bryan <tim.bryan@drpgroup.com>
2022-11-21 10:53:07 +0000

    use isBrowser util

--- src/hooks/useIsomorphicLayoutEffect.ts
+++ src/hooks/useIsomorphicLayoutEffect.ts
@@ -1,7 +1,7 @@
 /* C/O https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect */
+import { isBrowser } from '../utils/environment';
 import { useEffect, useLayoutEffect } from 'react';
 
-const useIsomorphicLayoutEffect =
-  typeof window !== 'undefined' ? useLayoutEffect : useEffect;
+const useIsomorphicLayoutEffect = isBrowser ? useLayoutEffect : useEffect;
 
 export default useIsomorphicLayoutEffect;
