f7b2b7bc7c9f724bee11e74fd95ac4265cd9c0c5
Tim Bryan <hi@timbryan.dev>
2023-09-13 11:12:20 +0100

    linting

--- src/pages/tim.js.tsx
+++ src/pages/tim.js.tsx
@@ -1,10 +1,22 @@
-import { Main } from '../layout/Main';
-import { Meta } from '../layout/Meta';
+import { Main } from '../layout/Main'
+import { Meta } from '../layout/Meta'
 
 const TimJS = (): JSX.Element => (
-  <Main meta={<Meta title="tim.js" description="The Input Manipulator dot JavaScript" />}>
-    <iframe style={{ position: 'absolute', top: 0, left: 0 }} width="100vw" height="100vh" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?si=L035Qqb0qBsd1K4q&autoplay=1" frameBorder="0" allow="autoplay" allowFullScreen></iframe>
+  <Main
+    meta={
+      <Meta title='tim.js' description='The Input Manipulator dot JavaScript' />
+    }
+  >
+    <iframe
+      style={{ position: 'absolute', top: 0, left: 0 }}
+      width='100vw'
+      height='100vh'
+      src='https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?si=L035Qqb0qBsd1K4q&autoplay=1'
+      frameBorder='0'
+      allow='autoplay'
+      allowFullScreen
+    ></iframe>
   </Main>
-);
+)
 
-export default TimJS;
+export default TimJS
